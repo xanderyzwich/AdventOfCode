c6552bcc62471c011837cc1c4819690880666a20
xanderyzwich <corey.d.mccarty@gmail.com>
2018-12-08 13:31:06 -0600

    Day 06 Completed

--- 06/script.py
+++ 06/script.py
@@ -12,47 +12,10 @@ def read_input():
     locations = []
     with open('input.txt', 'r') as input_file:
         for label, line in enumerate(list(input_file)):
-            x, y = re.split(', ', line[:-1])
+            x, y = re.split(', ', line)
             # print(label, ":", x, ',', y)
             locations.append([label, x, y])
-
     return locations
-
-
-def populate_map(locations):
-    mymap = np.zeros([500, 500])
-    counts = {}
-    infinite = []
-    for i in range(500):
-        for j in range(500):
-            distances = []
-            closest, distance = 0, 0
-            for name, x, y in locations:
-                distances.append([name, manhattan_distance([i, j], [x, y])])
-            distance_only = list(zip(*distances))[1]
-            least = min(distance_only)
-            if distance_only.count(least) == 1:
-                winner = distances[distance_only.index(least)][0]
-                mymap[i][j] = winner
-                if i in (0, 499) or j in (0, 499):
-                    infinite.append(winner)
-                else:
-                    if winner in counts.keys():
-                        counts[winner] += 1
-                    else:
-                        counts[winner] = 1
-
-    return mymap, counts, infinite
-
-
-def answer_part1(counts, infinite):
-    winner, most = 0, 0
-    for key in counts.keys():
-        total = counts.get(key)
-        # print(key, total)
-        if total > most and key not in infinite:
-            winner, most = key, total
-    print(winner, most)
 
 
 def manhattan_distance(point1, point2):
@@ -87,6 +50,55 @@ def minmax(locations_list):
     print('\t', 'Y goes from', miny, 'to', maxy)
 
 
+def part1_preprocess(locations):
+    mymap = np.zeros([500, 500])
+    counts = {}
+    infinite = []
+    for i in range(500):
+        for j in range(500):
+            distances = []
+            closest, distance = 0, 0
+            for name, x, y in locations:
+                distances.append([name, manhattan_distance([i, j], [x, y])])
+            distance_only = list(zip(*distances))[1]
+            least = min(distance_only)
+            if distance_only.count(least) == 1:
+                winner = distances[distance_only.index(least)][0]
+                mymap[i][j] = winner
+                if i in (0, 499) or j in (0, 499):
+                    infinite.append(winner)
+                else:
+                    if winner in counts.keys():
+                        counts[winner] += 1
+                    else:
+                        counts[winner] = 1
+
+    return mymap, counts, infinite
+
+
+def part1_answer(counts, infinite):
+    winner, most = 0, 0
+    for key in counts.keys():
+        total = counts.get(key)
+        # print(key, total)
+        if total > most and key not in infinite:
+            winner, most = key, total
+    print(winner, most)
+
+
+def part2(locations):
+    area = 0
+    for i in range(500):
+        for j in range(500):
+            manhattan_sum = 0
+            for name, x, y in locations:
+                # x, y = str(int(x) + 350), str(int(y) + 350)
+                manhattan_sum += manhattan_distance([i, j], [x, y])
+            if manhattan_sum < 10000:
+                area += 1
+    print("Part2: Safe area is ", area)  # 42939
+
+
 if __name__ == '__main__':
 
     print('Starting... ', end='')
@@ -99,9 +111,16 @@ if __name__ == '__main__':
     print('Used area of the map')
     minmax(locations)  # for planning and research
 
-    map_data, counts, infinite = populate_map(locations)
+    """
+    Do work for Part 1
+    """
+    map_data, counts, infinite = part1_preprocess(locations)
     print('map populated')
-    answer_part1(counts, infinite)
+    part1_answer(counts, infinite)
     print('part1 complete at ', dt.datetime.now() - start)
 
-
+    """
+    Do work for Part 2
+    """
+    part2(locations)
+    print('part2 complete at ', dt.datetime.now() - start)
