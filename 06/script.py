df847240d5486e0038018f2c11683eaa3c4061ff
xanderyzwich <corey.d.mccarty@gmail.com>
2018-12-08 12:06:39 -0600

    Day 06 (part 1 complete)

--- 06/script.py
+++ 06/script.py
@@ -19,41 +19,6 @@ def read_input():
     return locations
 
 
-def populate_map_old(locations):
-    mymap = np.zeros([500, 500])
-    counts = {}
-    infinite = []
-    print(len(locations.keys()))
-    for i in range(500):
-        for j in range(500):
-            distances = {}
-            closest, distance = 0, 0
-            for location in locations.keys():
-                # print(location)
-                x, y = locations.get(location)
-                far = manhattan_distance(i, j, int(x), int(y))
-                if far < distance:
-                    closest, distance = location, far
-                distances[location] = far
-            values = list(distances.values())
-            if values.count(min(values)) == 1: # no tie for closest
-                # print('no tie at', i, j)
-                mymap[i][j] = closest
-                if closest in counts.keys():
-                    counts[closest] = counts.get(closest) + 1
-                else:
-                    counts[closest] = 1
-                if (i in (0, 499) or j in (0, 499)) \
-                        and closest not in infinite:
-                    print("appending")
-                    infinite.append(closest)
-            else:
-                mymap[i][j] = None
-            # print('completed', i, j)
-    # print(mymap)
-    return mymap, counts, infinite
-
-
 def populate_map(locations):
     mymap = np.zeros([500, 500])
     counts = {}
@@ -77,14 +42,14 @@ def populate_map(locations):
                     else:
                         counts[winner] = 1
 
-    return map_data, counts, infinite
+    return mymap, counts, infinite
 
 
 def answer_part1(counts, infinite):
     winner, most = 0, 0
     for key in counts.keys():
         total = counts.get(key)
-        print(key, total)
+        # print(key, total)
         if total > most and key not in infinite:
             winner, most = key, total
     print(winner, most)
@@ -99,6 +64,7 @@ def manhattan_distance(point1, point2):
     yo = y2 - y1 if y2 > y1 else y1 - y2
     # print(xo, yo)
     return xo + yo
+
 
 def minmax(locations_list):
     """
@@ -131,7 +97,7 @@ if __name__ == '__main__':
     # X goes from 44 to 313
     # Y goes from 25 to 342
     print('Used area of the map')
-    minmax(locations) # for planning and research
+    minmax(locations)  # for planning and research
 
     map_data, counts, infinite = populate_map(locations)
     print('map populated')
