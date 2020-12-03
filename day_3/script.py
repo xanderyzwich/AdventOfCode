5a1bbfa3b033a1f97b912e84d1b7d842d6eb833d
Corey McCarty <corey.d.mccarty@gmail.com>
2020-12-03 13:58:10 -0600

    cleanup/refactor

--- day_3/script.py
+++ day_3/script.py
@@ -12,20 +12,15 @@ class TobogganMap:
             [self.map.append(row.replace('\n', '')) for row in input_file]
         self.width = len(self.map[0])
         self.height = len(self.map)
-        # print(f'Map created with {self.width} x {self.height} shape')
-        # print(self)
 
-    def slope_tree_count(self, x, y, right=3, down=1):
-        _x, _y, count = x+right, y+down, 0
+    def slope_tree_count(self, right=3, down=1):
+        _x, _y, count = right, down, 0
         while _y < self.height:
-            # print('spot = ', self.get(_x, _y))
             count += 1 if '#' == self.get(_x, _y) else 0
             _x, _y = _x+right, _y+down
-        # print(count)
         return count
 
     def get(self, x, y):
-        # print(x, y)
         return self.map[y][x % self.width]
 
     def __str__(self):
@@ -36,31 +31,32 @@ class TobogganMap:
             temp += '\n'
         return temp + '\n'
 
-def tobaggan_path_product(input_file_name):
-    my_map = TobogganMap(input_file_name)
-    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-    my_product = 1
-    for right, left in slopes:
-        my_product *= my_map.slope_tree_count(0, 0, right, left)
-    # print(my_product)
-    return my_product
+    @property
+    def paths_product(self):
+        slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        my_product = 1
+        for right, left in slopes:
+            my_product *= self.slope_tree_count(right, left)
+        return my_product
 
 
 class TestTobogganMap(TestCase):
 
     def test_one_example(self):
         my_map = TobogganMap('example.txt')
-        assert my_map.slope_tree_count(0, 0) == 7
+        assert my_map.slope_tree_count(3, 1) == 7
 
     def test_one_data(self):
         my_map = TobogganMap('data.txt')
-        assert my_map.slope_tree_count(0, 0) == 232
+        assert my_map.slope_tree_count(3, 1) == 232
 
     def test_two_example(self):
-        assert tobaggan_path_product('example.txt') == 336
+        my_map = TobogganMap('example.txt')
+        assert my_map.paths_product == 336
 
     def test_two_data(self):
-        assert tobaggan_path_product('data.txt') == 3952291680
+        my_map = TobogganMap('data.txt')
+        assert my_map.paths_product == 3952291680
 
 
 
