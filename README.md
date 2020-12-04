c9ada2f991bf896e5081c0097c628417dd419965
Corey McCarty <corey.d.mccarty@gmail.com>
2020-12-04 08:32:24 -0600

    add generic script format.py

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Directories separate the days so that attempts can easily be made with multiple 
 
 
 I typically use Python for puzzle solutions, but I will hopefully find time to solve some with Go.
+
+
+format.py defines the general format for my script.py for more quickly starting each day.
---
+++ format.py
@@ -0,0 +1,23 @@
+"""
+Day X: Name
+"""
+from unittest import TestCase
+
+
+def thing(stuff):
+    pass
+
+
+class TestThing(TestCase):
+
+    def test_one_example(self):
+        assert True
+
+    def test_one_data(self):
+        assert True
+
+    def test_two_example(self):
+        assert True
+
+    def test_two_data(self):
+        assert True
