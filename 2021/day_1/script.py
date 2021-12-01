b98999cd2946f6a5750da72c894ffebf8d31e608
Corey McCarty <Corey.McCarty@juiceplus.com>
2021-12-01 10:11:34 -0600

    refactored testing for pass/fail

--- 2021/day_1/script.py
+++ 2021/day_1/script.py
@@ -31,25 +31,39 @@ def sliding_window_sum(nums, window_size=3):
 class TestDay1(TestCase):
     example = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        print(f'\nExecuting {cls.__name__}')
+
     def setUp(self) -> None:
-        print(f'\n--- Running test: {self._testMethodName} ---')
+        self.current_result = False
+        print(f'\t Running {self._testMethodName}', end='\n\t\t')
+
+    def assertion(self, test_passes) -> None:
+        self.current_result = test_passes
+        assert test_passes
+
+    def tearDown(self) -> None:
+        test_result = 'PASS' if self.current_result else 'FAIL'
+        self.current_result = False
+        print('\t\t' + test_result)
 
     def test_one_example(self):
-        assert count_depth_increases(self.example) == 7
+        self.assertion(count_depth_increases(self.example) == 7)
 
     def test_one_data(self):
         data = int_list_from_file('data.txt')
         result = count_depth_increases(data)
-        assert 1266 == result
+        self.assertion(1266 == result)
 
     def test_two_example(self):
         sums = sliding_window_sum(self.example)
-        assert sums == [607, 618, 618, 617, 647, 716, 769, 792]
+        self.assertion(sums == [607, 618, 618, 617, 647, 716, 769, 792])
         result = count_depth_increases(sums)
-        assert 5 == result
+        self.assertion(5 == result)
 
     def test_two_data(self):
         data = int_list_from_file('data.txt')
         sums = sliding_window_sum(data)
         result = count_depth_increases(sums)
-        assert 1217 == result
+        self.assertion(1217 == result)
--- format.py
+++ format.py
@@ -10,17 +10,31 @@ def thing(stuff):
 
 class TestThing(TestCase):
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        print(f'\nExecuting {cls.__name__}')
+
     def setUp(self) -> None:
-        print(f'\n--- Running test: {self._testMethodName} ---')
+        self.current_result = False
+        print(f'\t Running {self._testMethodName}', end='\n\t\t')
+
+    def assertion(self, test_passes) -> None:
+        self.current_result = test_passes
+        assert test_passes
+
+    def tearDown(self) -> None:
+        test_result = 'PASS' if self.current_result else 'FAIL'
+        self.current_result = False
+        print('\t\t' + test_result)
 
     def test_one_example(self):
-        assert True
+        self.assertion(True)
 
     def test_one_data(self):
-        assert True
+        self.assertion(True)
 
     def test_two_example(self):
-        assert True
+        self.assertion(True)
 
     def test_two_data(self):
-        assert True
+        self.assertion(True)
